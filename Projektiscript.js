c74cc0f65865c6ae7528154602c5b5ada5fe7025
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2021-08-24 15:52:35 +0000

    20210824

--- Projektiscript.js
+++ Projektiscript.js
@@ -1,3 +1,14 @@
-document.guerySelector('lomake2').addEventListener('click'e=>{
+//console.log(document.querySelector('#Nimi').type):
+//console.log(document.querySelector('.lomake).value);
+document.querySelector('.lomake').addEventListener('click', e=>{e.preventDefault();
+
+
+document.guerySelector('.vastaus').addEventListener('click',e=>{
   e.preventDefault();
 console.log("tunnit pidetty!");
+
+if(document.querySelector('#Hmtl').checked){document.querySelector('lomakee').children[0].innerHTML='<div class="lomakee"><input type="checkbox"id="Html"checked>HTML tunnit pidetty!</div>';
+}
+  else{
+    document.querySelector('.Html').children[0].innerHTML = '<div class="lomakee"><input type="checbox"id="Html">HTML</div>';
+  }
--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function laskutehtava(ekaArvo,tokaArvo){
-  return ekaArvo + tokaArvo;
+ return ekaArvo + tokaArvo;
 }
 //function pvmfin(teksti,vuosi,kk,pv){
-//  return teksti+"."pv+"."+kk+"."+vuosi;
+ // return teksti+"."pv+"."+kk+"."+vuosi;
 //}
-function pvmfin(vuosi, kk, pv){
-  if(kk==03|| kk==04|| kk==05){
-  return "kevät:"+pv+"."+kk+"."+vuosi;
-}
-function pvmfin(vuosi,kk,pv){
-  if(kk==06|| kk==07|| kk==08){
-  return "kesä:"+pv+"."+kk+"."+vuosi;
-}
-function pvmfin(vuosi,kk,pv){
-  if(kk==09|| kk==10|| kk==11){
-  return "syksy:"+pv+"."+kk+"."+vuosi;
-}
-  function pvmfin(vuosi,kk,pv){
-  if(kk==12|| kk==01|| kk==02){
-  return "Talvi:"+pv+"."+kk+"."+vuosi;
-}
-}
+//function pvmfin(vuosi, kk, pv){
+ //if(kk==03|| kk==04|| kk==05){
+ // return "kevät:"+pv+"."+kk+"."+vuosi;
+//
+//function pvmfin(vuosi,kk,pv){
+//  if(kk==06|| kk==07|| kk==08){
+//  return "kesä:"+pv+"."+kk+"."+vuosi;
+//}
+//function pvmfin(vuosi,kk,pv){
+ // if(kk==09|| kk==10|| kk==11){
+// return "syksy:"+pv+"."+kk+"."+vuosi;
+//}
+//function pvmfin(vuosi,kk,pv){
+ // if(kk==12|| kk==01|| kk==02){
+//  return "Talvi:"+pv+"."+kk+"."+vuosi;
+//}
+//}
 console.log("Täällä ollaan!");
 let ika=44;//numerot kirjoitetaan ilman lainausmerkkejä
 ika= ika+1;//integer (int)
@@ -55,7 +55,7 @@ else if(ika=== 18 && nimi =="Keijo"){
   console.log("Onnea Keijo, olet juuri ja juuri täysi-ikäinen!");
 }
 else{
-  console.error("kasva vielä vähän.....");
+ console.error("kasva vielä vähän.....");
 }
 
 console.log(laskutehtava(927874287,9487342));
@@ -65,7 +65,8 @@ let pvm="2021/08/18";
 console.log(pvmfin(pvm.split("/")[0],pvm.split("/")[1],pvm.split("/")[2],pvm.split("/")));
  
 for(let i=1; i<=15;i++){
-  console.log(pvm);
+  console.log(i+""+pvmFin(pvm.split("/")[0],pvm.split("/")[1],pvm.split("/")[2]
+  ));
 }
 
 
@@ -80,27 +81,27 @@ kommentti
 //console.log(document.querySelector('#kentta').type);
 //console.log(document.guerySelector('lisaanappi').value);
 
-document.guerySelector('.lisaanappi').addEventListener('clikc',e=>{e.preventDefault();
+//document.guerySelector('.lisaanappi').addEventListener('clikc',e=>{e.preventDefault();
 
 //console.log(document.guerySelector('#kentta).value);
 
-document.guerySelector('.ilmoitus').innerHTML="<b>Moikka</b>"+document.querySelector('#kentta').value;
+//document.guerySelector('.ilmoitus').innerHTML="<b>Moikka</b>"+document.querySelector('#kentta').value;
 
-setTimeout(() => document.querySelector('.ilmoitus').innerHTML='',3000);
-});
+//setTimeout(() => document.querySelector('.ilmoitus').innerHTML='',3000);
+//});
 
-document.querySelector('.merkinta').addEventListener('click',e =>{
-  e.preventDefault();
-  console.log("nappi toimii!");
+//document.querySelector('.merkinta').addEventListener('click',e =>{
+ // e.preventDefault();
+ // console.log("nappi toimii!");
 
-  if(document.querySelector('#teht_a').checked){document.querySelector('.boksit').children[0].inner.innerHTML='<li class"sisalto"><input type="checkbox id="teht_a"checked>Tehtävä A ontehty!</li>';
-  }
-  else{
-    document.guerySelector('.boksit').children[0].innerHTML='<li class="sisalto"><input type="checkbox" id="teht_a">Tehtävä A</li>';
-  }
-   if(document.querySelector('#teht_b').checked){document.querySelector('.boksit').children[0].inner.innerHTML='<li class"sisalto"><input type="checkbox id="teht_b"checked>Tehtävä B ontehty!</li>';
-  }
-  else{
-    document.guerySelector('.boksit').children[0].innerHTML='<li class="sisalto"><input type="checkbox" id="teht_b">Tehtävä B</li>';
-  }
-});
+  //if(document.querySelector('#teht_a').checked){document.querySelector('.boksit').children[0].inner.innerHTML='<li class"sisalto"><input type="checkbox id="teht_a"checked>Tehtävä A on tehty!</li>';
+ // }
+  //else{
+   // document.guerySelector('.boksit').children[0].innerHTML='<li class="sisalto"><input type="checkbox" id="teht_a">Tehtävä A</li>';
+ // }
+  // if(document.querySelector('#teht_b').checked){document.querySelector('.boksit').children[0].inner.innerHTML='<li class"sisalto"><input type="checkbox id="teht_b"checked>Tehtävä B on tehty!</li>';
+  //}
+ // else{
+  //  document.guerySelector('.boksit').children[0].innerHTML='<li class="sisalto"><input type="checkbox" id="teht_b">Tehtävä B</li>';
+//  }
+//});
