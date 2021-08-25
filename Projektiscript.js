ecd70f54a27718331569d00c6dc04efd5a58eaf4
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2021-08-25 11:36:00 +0000

    20210825

--- Projektiscript.js
+++ Projektiscript.js
@@ -1,14 +1,49 @@
-//console.log(document.querySelector('#Nimi').type):
+
+//console.log(document.querySelector('#Nimi').type);
 //console.log(document.querySelector('.lomake').value);
-document.querySelector('.lomakee').addEventListener('click', e=>{e.preventDefault();
-console.log("toimii");
-});
+
+//document.querySelector('.lomake').addEventListener('click', e=>{e.preventDefault();
+//console.log(document.querySelector('#Nimi').value);
+
+//document.querySelector('.lomakee').addEventListener('click', e=>{e.preventDefault();
+//console.log("toimii");
+//});
 document.querySelector('.vastaus').addEventListener('click',e=>{
   e.preventDefault();
-console.log("tunnit pidetty!");
+//console.log("tunnit pidetty!");
+
+if(document.querySelector('#Html').checked){
+  console.log('hip');
+  document.querySelector('.lomakee').children[0].innerHTML='<li class="lomakee"><input type="checkbox" id="Html" checked>HTML tunnit pidetty!</li>';
+}
+else{
+  console.log('hop');
+  document.querySelector('.lomakee').children[0].innerHTML ='<li class="lomakee"><input type="checkbox"id="Html">HTML</li>';
+}
+
+if(document.querySelector('#CSS').checked){
+  console.log('hip');
+  document.querySelector('.lomakee').children[1].innerHTML='<li class="lomakee"><input type="checkbox" id="CSS" checked>CSS tunnit pidetty!</li>';
+}
+else{
+  console.log('hop');
+  document.querySelector('.lomakee').children[1].innerHTML ='<li class="lomakee"><input type="checkbox"id="CSS">CSS</li>';
+}
+if(document.querySelector('#JavaScript').checked){
+  console.log('hip');
+  document.querySelector('.lomakee').children[2].innerHTML='<li class="lomakee"><input type="checkbox" id="JavaScript" checked>JavaScript tunnit pidetty!</li>';
+}
+else{
+  console.log('hop');
+  document.querySelector('.lomakee').children[2].innerHTML ='<li class="lomakee"><input type="checkbox"id="JavaScript">JavaScript</li>';
+}
+if(document.querySelector('#CICD').checked){
+  console.log('hip');
+  document.querySelector('.lomakee').children[3].innerHTML='<li class="lomakee"><input type="checkbox" id="CICD" checked>CI/CD tunnit pidetty!</li>';
+}
+else{
+  console.log('hop');
+  document.querySelector('.lomakee').children[3].innerHTML ='<li class="lomakee"><input type="checkbox"id="CICD">CI/CD</li>';
+}
 });
-if(document.querySelector('#Html').checked){document.querySelector('lomakee').children[0].innerHTML='<div class="lomakee"><input type="checkbox"id="Html"checked>HTML tunnit pidetty!</div>';
-}
-  else{
-    document.querySelector('.ilmoitus').children[0].innerHTML ='<div class="lomakee"><input type="checbox"id="Html">HTML</div>';
-}
+
