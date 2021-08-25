7402e318e5a4fe4ce30916ef4ad1f3d623e026b0
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2021-08-25 16:17:37 +0000

    20210825

--- Projektiscript.js
+++ Projektiscript.js
@@ -2,9 +2,12 @@
 //console.log(document.querySelector('#Nimi').type);
 //console.log(document.querySelector('.lomake').value);
 
-//document.querySelector('.lomake').addEventListener('click', e=>{e.preventDefault();
-//console.log(document.querySelector('#Nimi').value);
+document.querySelector('.lomake').addEventListener('click', e=>{e.preventDefault();
+console.log(document.querySelector('#Nimi').value);
 
+document.querySelector('.laatikko').innerHTML=
+"<b>da daa:</b>"+document.querySelector('#Nimi').value;
+}
 //document.querySelector('.lomakee').addEventListener('click', e=>{e.preventDefault();
 //console.log("toimii");
 //});
@@ -46,4 +49,3 @@ else{
   document.querySelector('.lomakee').children[3].innerHTML ='<li class="lomakee"><input type="checkbox"id="CICD">CI/CD</li>';
 }
 });
-
