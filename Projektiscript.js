7d68a59a12c1ccc829aa4a684aac9a6b12b6b15b
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2021-08-25 07:15:50 +0000

    20210825

--- Projektiscript.js
+++ Projektiscript.js
@@ -1,15 +1,14 @@
 //console.log(document.querySelector('#Nimi').type):
-//console.log(document.querySelector('.lomake).value);
-document.querySelector('.lomake').addEventListener('click', e=>{e.preventDefault();
-console.log("nappula toimii");
-
-
-//document.guerySelector('.vastaus').addEventListener('click',e=>{
- // e.preventDefault();
-//console.log("tunnit pidetty!");
-
-//if(document.querySelector('#Html').checked){document.querySelector('lomakee').children[0].innerHTML='<div class="lomakee"><input type="checkbox"id="Html"checked>HTML tunnit pidetty!</div>';
-//}
- // else{
-   // document.querySelector('.ilmoitus').children[0].innerHTML = '<div class="lomakee"><input type="checbox"id="Html">HTML</div>';
-//}
+//console.log(document.querySelector('.lomake').value);
+document.querySelector('.lomakee').addEventListener('click', e=>{e.preventDefault();
+console.log("toimii");
+});
+document.querySelector('.vastaus').addEventListener('click',e=>{
+  e.preventDefault();
+console.log("tunnit pidetty!");
+});
+if(document.querySelector('#Html').checked){document.querySelector('lomakee').children[0].innerHTML='<div class="lomakee"><input type="checkbox"id="Html"checked>HTML tunnit pidetty!</div>';
+}
+  else{
+    document.querySelector('.ilmoitus').children[0].innerHTML ='<div class="lomakee"><input type="checbox"id="Html">HTML</div>';
+}
